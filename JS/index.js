c9a760bcfeae27bde169a19b89b821b8869b8ce9
carmenwdesign <carmenwdesign@gmail.com>
2023-07-10 17:16:14 +0100

    Version 4

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
-    function fade() {
-      $('.preloader').fadeOut('slow');
-    }
-    setTimeout(fade, 3000);
-  });
+  // Hide the navbar initially
+  $('.navbarCS').hide();
+  $('footer').hide();
+
+  function fade() {
+    $('.cube-loader').fadeOut('slow', function() {
+      // Animation complete callback
+      // Show the navbar and rest of the content
+      $('.navbarCS').fadeIn('slow');
+      $('#content').fadeIn('slow');
+      $('footer').fadeIn('slow');
+    });
+  }
+  setTimeout(fade, 3000);
+
+    $('li.navbar-item.work a').click(function(e) {
+      e.preventDefault();
+      var targetSection = $(this).attr('href');
+      $('html, body').animate({
+        scrollTop: $(targetSection).offset().top
+      }, 1000);
+    }); 
+
+    $('#top-project').click(function() {
+      window.location.href = 'case_study.html';
+    });
+
+    var welcomeArrow = $('<div class="welcome-arrow"></div>').appendTo('body');
+    welcomeArrow.css('display', 'inline-block');
   
+    welcomeArrow.hover(function() {
+      $(this).css('animation-play-state', 'paused');
+    }, function() {
+      $(this).css('animation-play-state', 'running');
+    });
+
+});
+
